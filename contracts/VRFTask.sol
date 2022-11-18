5d2b8c7d052bf0425cca89f8ee77d6e1ae2d772c
李正星 <lzx20161004195@163.com>
2022-11-18 18:51:29 +0800

    完成任务

--- contracts/VRFTask.sol
+++ contracts/VRFTask.sol
@@ -1,4 +1,3 @@
-
 // An example of a consumer contract that relies on a subscription for funding.
 
 pragma solidity ^0.8.0;
@@ -12,34 +11,33 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
  * 通过 fulfillRandomWords 函数给 s_randomness[] 填入 5 个随机数
  * 保证 5 个随机数为 5 以内，并且不重复
  * 参考视频教程： https://www.bilibili.com/video/BV1ed4y1N7Uv
- * 
+ *
  * 任务 2 完成标志：
  * 1. 通过命令 "yarn hardhat test" 使得单元测试 7-9 通过
  * 2. 通过 Remix 在 goerli 测试网部署，并且测试执行是否如预期
-*/
-
+ */
 
 contract VRFTask is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface immutable COORDINATOR;
-    
-    /* 
+
+    /*
      * 步骤 1 - 获得 VRFCoordinator 合约的地址和所对应的 keyHash
      * 修改变量
      *   CALL_BACK_LIMIT：回调函数最大 gas 数量
      *   REQUEST_CONFIRMATIONS：最小确认区块数
      *   NUM_WORDS：单次申请随机数的数量
-     * 
+     *
      * 注意：
-     * 通过 Remix 部署在非本地环境时，相关参数请查看 
+     * 通过 Remix 部署在非本地环境时，相关参数请查看
      * https://docs.chain.link/docs/vrf/v2/supported-networks/，获取 keyHash 的指和 vrfCoordinator 的地址
      * 本地环境在测试脚本中已经自动配置
-     * 
-     */ 
+     *
+     */
     uint64 immutable s_subscriptionId;
     bytes32 immutable s_keyHash;
-    uint32 constant CALL_BACK_LIMIT = 100;
-    uint16 constant REQUEST_CONFIRMATIONS = 1;
-    uint32 constant NUM_WORDS = 1;
+    uint32 constant CALL_BACK_LIMIT = 200_000;
+    uint16 constant REQUEST_CONFIRMATIONS = 6;
+    uint32 constant NUM_WORDS = 5;
 
     uint256[] public s_randomWords;
     uint256 public s_requestId;
@@ -48,12 +46,12 @@ contract VRFTask is VRFConsumerBaseV2 {
 
     event ReturnedRandomness(uint256[] randomWords);
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == s_owner);
         _;
     }
 
-    /**  
+    /**
      * 步骤 2 - 在构造函数中，初始化相关变量
      * COORDINATOR，s_subscriptionId 和 s_keyHash
      * */
@@ -63,31 +61,65 @@ contract VRFTask is VRFConsumerBaseV2 {
         bytes32 _keyHash
     ) VRFConsumerBaseV2(vrfCoordinator) {
         s_owner = msg.sender;
-        
+
         //修改以下 solidity 代码
-        COORDINATOR = VRFCoordinatorV2Interface(address(0));
-        s_subscriptionId = 0;
-        s_keyHash = "";
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_subscriptionId = _subscriptionId;
+        s_keyHash = _keyHash;
     }
 
-    /** 
+    /**
      * 步骤 3 - 发送随机数请求
-     * */ 
+     * */
     function requestRandomWords() external onlyOwner {
         //在此添加并且修改 solidity 代码
+        s_requestId = COORDINATOR.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            CALL_BACK_LIMIT,
+            NUM_WORDS
+        );
     }
 
     /**
      * 步骤 4 - 接受随机数，完成逻辑获取 5 个 5 以内**不重复**的随机数
      * 关于如何使得获取的随机数不重复，清参考以下代码
      * https://gist.github.com/cleanunicorn/d27484a2488e0eecec8ce23a0ad4f20b
-     *  */ 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory _randomWords)
-        internal
-        override
-    {
+     *  */
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory _randomWords
+    ) internal override {
         //在此添加 solidity 代码
-        
+        s_randomWords = new uint[](NUM_WORDS);
+
+        // Initialize array.
+        for (uint i = 0; i < NUM_WORDS; i++) {
+            s_randomWords[i] = i + 1;
+        }
+        // Set the last item of the array which will be swapped.
+        uint last_item = NUM_WORDS - 1;
+        // Set the initial randomness based on the provided entropy.
+        bytes32 random = keccak256(abi.encodePacked(_randomWords[last_item]));
+        // We need to do `size - 1` iterations to completely shuffle the array.
+        for (uint i = 1; i < NUM_WORDS - 1; i++) {
+            // Select a number based on the randomness.
+            uint selected_item = uint(random) % last_item;
+
+            // Swap items `selected_item <> last_item`.
+            uint aux = s_randomWords[last_item];
+            s_randomWords[last_item] = s_randomWords[selected_item];
+            s_randomWords[selected_item] = aux;
+
+            // Decrease the size of the possible shuffle
+            // to preserve the already shuffled items.
+            // The already shuffled items are at the end of the array.
+            last_item--;
+
+            // Generate new randomness.
+            random = keccak256(abi.encodePacked(_randomWords[last_item]));
+        }
         emit ReturnedRandomness(s_randomWords);
     }
 }
--- test/2_AutomationTest.js
+++ test/2_AutomationTest.js
@@ -1,33 +1,44 @@
-const { ethers } = require("hardhat");
+/*
+ * @Descripttion:
+ * @Author: lizhengxing
+ * @Date: 2022-11-18 13:38:13
+ * @LastEditTime: 2022-11-18 18:05:15
+ */
+const { ethers, network } = require("hardhat");
 const { assert, expect } = require("chai");
-const { loadFixture, time } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("单元测试：Chainlink Automation", async function() {
-    async function deployAutomationFixture() {
-        const automationContract = await ethers.getContractFactory("AutomationTask");
-        const interval = "120";
-        const automation = await automationContract.deploy(interval);
-        return { automation };
-    }
+describe("单元测试：Chainlink Automation", async function () {
+  async function deployAutomationFixture() {
+    const automationContract = await ethers.getContractFactory(
+      "AutomationTask"
+    );
+    const interval = "120";
+    const automation = await automationContract.deploy(interval);
+    return { automation };
+  }
 
-    it("单元测试 10： 状态未发生变化时，checkUpkeep 返回 false", async function() {
-        const { automation } = await loadFixture(deployAutomationFixture);
-        const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
-        const { upkeepNeeded } = await automation.callStatic.checkUpkeep(checkData);
-        assert.equal(upkeepNeeded, false);
-    });
+  it("单元测试 10： 状态未发生变化时，checkUpkeep 返回 false", async function () {
+    const { automation } = await deployAutomationFixture();
+    const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
+    const { upkeepNeeded } = await automation.callStatic.checkUpkeep(checkData);
+    assert.equal(upkeepNeeded, false);
+  });
 
-    it("单元测试 11：满足 checkUpkeep 状态时，成功执行 performUpkeep ", async function() {
-        const { automation } = await loadFixture(deployAutomationFixture);
-        const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
-        
-        const interval = await automation.interval();
-        await time.increase(interval.toNumber() + 1);
-        await automation.fight(1);
-        await automation.performUpkeep(checkData);
-        const health = await automation.healthPoint(1);
-        assert(health == 1000, "the health is not refreshed to 1000");
+  it("单元测试 11：满足 checkUpkeep 状态时，成功执行 performUpkeep ", async function () {
+    const { automation } = await deployAutomationFixture();
+    const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
 
-    });
+    await automation.fight(1);
 
+    const interval = await automation.interval();
+    await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+    await network.provider.send("evm_mine", []);
+    // await time.increase(interval.toNumber() + 1);
+    const { upkeepNeeded, performData } =
+      await automation.callStatic.checkUpkeep(checkData);
+    await automation.performUpkeep(performData);
+    const health = await automation.healthPoint(1);
+    // console.log("health: ", health);
+    assert(health == 1000, "the health is not refreshed to 1000");
+  });
 });
